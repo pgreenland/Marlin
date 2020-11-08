1f7a463a59b43f98dc14f373214b37efb3560646
Phil Greenland <phil@quantulum.co.uk>
2020-11-08 21:20:27 +0000

    Reduce laser PWM frequency to match mini gerbil.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2868,7 +2868,7 @@
   #define SPINDLE_LASER_PWM             true   // Set to "true" if your controller supports setting the speed/power
   #define SPINDLE_LASER_PWM_INVERT      false  // Set to "true" if the speed/power goes up when you want it to go slower
 
-  #define SPINDLE_LASER_FREQUENCY       2500   // (Hz) (200us) Spindle/laser frequency (only on supported HALs: AVR and LPC)
+  #define SPINDLE_LASER_FREQUENCY       244   // (Hz) Spindle/laser frequency (only on supported HALs: AVR and LPC)
 
   /**
    * Speed / Power can be set ('M3 S') and displayed in terms of:
